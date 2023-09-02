70363809585bf3139b420983a4939ece788b6646
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-09-02 15:46:29 +0100

    Completed Todo note exercise task 6
    Put together code from task 1 to 5.
    Add a delete function to both new note items by attaching a click listener
    on creation
    Also added a delete function for any exising notes, by grabbing the existing
    list items and adding delete function listener to them
    
    Added a priority images object to each note depending on which priority user
    select = the object key.

--- Task_6/app.js
+++ Task_6/app.js
@@ -4,36 +4,72 @@ const priorityImages = {
   medium: 'https://cdn1.iconfinder.com/data/icons/prettyoffice8/256/Flag-yellow.png',
   high: 'https://cdn1.iconfinder.com/data/icons/prettyoffice8/256/Flag-red.png',
 };
+//Check for existing items in list
+const todoList = document.querySelectorAll('.todo');
+console.log(todoList);
+//Select the whole form element
+const form = document.querySelector('form');
+//Select the title input element
+const titleInput = form.elements.title;
+//Select the priority selection element
+const prioritySelect = form.elements.priority;
 
-// Get the form by ID from the forms collection.
-const form = false;
-// Get the todo pane (the 'ul' element) to insert todos into.
-const todoPane = false;
-// Get the text input for the title. We'll read from this when creating the todo.
-const titleInput = false;
-// Get the priority select element. We'll read from this when creating the todo.
-const prioritySelect = false;
-// Get a *live* list of all elements with the 'todo' class.
-const allTodos = false;
+//List related elements
+//Ul element
+const todoPane = document.getElementById('todo-pane');
+const allTodos = document.getElementsByClassName('todo');
 
 // Add an event listener that will
 // 1. Create a new todo. The details should come from the form.
 // 2. Insert it into the DOM.
 // 3. Clear the title input ready to create a new todo note.
 // 4. Prevent the default behaviour (ie don't submit to a server).
-
-function createTodo(title) {
-  // Create the text node with the variable 'title'.
+form.addEventListener('submit', (e) => {
+  //Prevent form submition
+  e.preventDefault();
   
-  // Create a new list item element to contain the text node.
+  //Create new todo list item from user input and add to ul list
+  //reset form after to refresh the input
+  const newTodoItem = createTodo(titleInput.value, prioritySelect.value);
+  todoPane.appendChild(newTodoItem);
+  form.reset();
   
-  // Add the text node to the list item element.
-  
-  // Return the new element.
-}
+  //Add a click listener to new to do item, and if clicked delete it by calling delete function  
+  newTodoItem.addEventListener('click', () => {
+  deleteTodoItem(newTodoItem);
+  });
+});
 
-// The most recently created list item (only!) should have the 'just-created' class.
+ createTodo = (title,priority) => {
+       
+        // Create the text node with the variable 'title'.
+        const titleTextNode = document.createTextNode(title);
+        // Create a new list item element to contain the text node.
+        const noteDivNode = document.createElement("div");
+        noteDivNode.appendChild(titleTextNode);  
+        const newListItem = document.createElement('li');
+        const priorityImage = document.createElement('img');
+        //The image src will be the priority value of low.medium, high as assigned in the object array
+        priorityImage.src = priorityImages[priority];
+       //Append note priority image, and text to li
+        newListItem.appendChild(priorityImage);
+        newListItem.appendChild(noteDivNode);
+        //Give new note class name todo
+        newListItem.classList.add('todo','just-created');
+        
+        // Return the new note.
+        return newListItem;
+      }
+ 
+//Function to delete item
+ function deleteTodoItem (toDoItem) {
+   toDoItem.remove();
+ } 
 
-// Optional extras:
-// 1. Add an image for the priority. The URLs are stored in the priorityImages object.
-// 1. Add a click handler to the note that deletes it on click using Element.remove().
+//Listen for and delete existing items if found in list on page refresh
+ todoList.forEach(todoItem => {
+      todoItem.addEventListener('click', (event) => {
+       deleteTodoItem(todoItem);
+      });
+    });
+ 
